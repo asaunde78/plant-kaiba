f58da8d71a9a918b852d515a9fa349e0e63249f6
asaunde78 <asaunde78@gmail.com>
2025-11-18 14:46:32 +0000

    refactored big nested loop

--- src/videograbbing/kaiba.py
+++ src/videograbbing/kaiba.py
@@ -53,73 +53,76 @@ def insta():
         body = {"content":"test"}
         entry = request.get_json().get("entry")
         
-        if (entry and entry[0]) :
-            mes = entry[0].get("messaging")
-            print(json.dumps(mes, indent=4))
-            if(mes and mes[0]):
-                message = mes[0].get("message")
-                if (message):
-                    attach = message.get("attachments")
-                    if(attach and attach[0]):
-                        payload = attach[0].get("payload")
-                        if(payload):
-                            
-                            url = payload.get("url")
-                            title =  payload.get("title")
-                            stream = min_cli.list_objects("zink", "kaiba", True)
-                            print(stream)
-                            part_size = 10 * 1024 * 1024
-                            with urllib.request.urlopen(url) as response:
-                                tags = Tags(for_object=True)
-                                user_id = mes[0].get("sender").get("id")
-                                tags["User"] = user_id
-                                user_data = igurl.get(user_id).json()
-                                print(user_data)
-                                tags["Timestamp"] = str(mes[0].get("timestamp")) 
-                                # print(response.headers)
-                                content_type = response.headers.get_content_type().split("/")[-1]
-                                u = urlparse(url)
-                                q = parse_qs(u.query)
-                                f_name = "temp"
-                                if("asset_id" in q):
-                                    f_name = q["asset_id"][0]
-                                # print("file name:",f_name)
-                                data = response
-                                arr = io.BytesIO()
-                                copy = io.BytesIO()
-                                while True:
-                                    # Read a chunk of data from the response
-                                    chunk = data.read(200)
-                                    # print(chunk)
-                                    if not chunk:
-                                        # Break the loop if no more data is received
-                                        break
-                                    # Write the chunk to the BytesIO buffer
-                                    arr.write(chunk)
-                                    copy.write(chunk)
-                            
-                                arr.seek(0)
-                                copy.seek(0)
-                                f = discord.File(arr, filename=f"temp.{content_type}")
-                                min_cli.put_object("zink", f"kaiba/{f_name}.{content_type}", copy, length=-1, part_size=part_size, tags=tags)
-                                body = {"content": f"Sent by: {user_data.get("username")} \n [{title if title else "no title"}]({url})"}
-                                
-                                
-                                
-                                
-                                
-                                embed = discord.Embed( description= title if title else "no title", timestamp=datetime.fromtimestamp(int(mes[0].get("timestamp"))/1000, tz=timezone("US/Eastern")))
-                                embed.set_author(name=user_data.get("username"),icon_url=user_data.get("profile_pic"))
-                                
-                                
-                                print(response.status, response.url, response.headers)
-                                
-                                webhook.send(file=f, embed=embed)
-                                body = {"content":f"[content]({url})", "embeds":[embed.to_dict()]}
-                                # print("E TO D", body)
-                                # res = requests.post(os.getenv("WEBHOOK"), json = body)
-                                # res = requests.post(os.getenv("WEBHOOK"), json = body, headers={"Content-Type":"application/json"})
-                                # print(res, res.reason, res.text)
+        if (not entry or  not entry[0]) :
+            return Response(status=500, mimetype='application/json')
+        mes = entry[0].get("messaging")
+        print(json.dumps(mes, indent=4))
+        if(not mes or not mes[0]):
+            return Response(status=500, mimetype='application/json')
+        message = mes[0].get("message")
+        if (not message):
+            return Response(status=500, mimetype='application/json')
+        attach = message.get("attachments")
+        if(not attach or not attach[0]):
+            return Response(status=500, mimetype='application/json')
+        payload = attach[0].get("payload")
+        if(not payload):
+            return Response(status=500, mimetype='application/json')
+            
+        url = payload.get("url")
+        title =  payload.get("title")
+        stream = min_cli.list_objects("zink", "kaiba", True)
+        print(stream)
+        part_size = 10 * 1024 * 1024
+        with urllib.request.urlopen(url) as response:
+            tags = Tags(for_object=True)
+            user_id = mes[0].get("sender").get("id")
+            tags["User"] = user_id
+            user_data = igurl.get(user_id).json()
+            print(user_data)
+            tags["Timestamp"] = str(mes[0].get("timestamp")) 
+            # print(response.headers)
+            content_type = response.headers.get_content_type().split("/")[-1]
+            u = urlparse(url)
+            q = parse_qs(u.query)
+            f_name = "temp"
+            if("asset_id" in q):
+                f_name = q["asset_id"][0]
+            # print("file name:",f_name)
+            data = response
+            arr = io.BytesIO()
+            copy = io.BytesIO()
+            while True:
+                # Read a chunk of data from the response
+                chunk = data.read(200)
+                # print(chunk)
+                if not chunk:
+                    # Break the loop if no more data is received
+                    break
+                # Write the chunk to the BytesIO buffer
+                arr.write(chunk)
+                copy.write(chunk)
+        
+            arr.seek(0)
+            copy.seek(0)
+            f = discord.File(arr, filename=f"temp.{content_type}")
+            min_cli.put_object("zink", f"kaiba/{f_name}.{content_type}", copy, length=-1, part_size=part_size, tags=tags)
+            body = {"content": f"Sent by: {user_data.get("username")} \n [{title if title else "no title"}]({url})"}
+            
+        
+            
+            embed = discord.Embed( description= title if title else "no title", timestamp=datetime.fromtimestamp(int(mes[0].get("timestamp"))/1000, tz=timezone("US/Eastern")))
+            embed.set_author(name=user_data.get("username"),icon_url=user_data.get("profile_pic"))
+            
+            
+            print(response.status, response.url, response.headers)
+            
+            webhook.send(file=f, embed=embed)
+            body = {"content":f"[content]({url})", "embeds":[embed.to_dict()]}
+            # print("E TO D", body)
+            # res = requests.post(os.getenv("WEBHOOK"), json = body)
+            # res = requests.post(os.getenv("WEBHOOK"), json = body, headers={"Content-Type":"application/json"})
+            # print(res, res.reason, res.text)
         return Response(status=200, mimetype='application/json')
     if(request.method=="GET"):
         print(request.args)
