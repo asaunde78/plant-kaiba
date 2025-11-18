2630972e1424bd46cad82222fa4d8c73465143a2
asaunde78 <asaunde78@gmail.com>
2025-11-18 14:48:58 +0000

    cleaned up

--- src/videograbbing/kaiba.py
+++ src/videograbbing/kaiba.py
@@ -1,14 +1,18 @@
-import requests
+
 from dotenv import load_dotenv
 import json
 import os 
+
+import requests
 import urllib.request
 from urllib.parse import urlparse, parse_qs
-import ffmpeg
+
 import discord 
 import json 
+
 from datetime import datetime, tzinfo
 from pytz import timezone 
+
 import io 
 
 import aiohttp
@@ -28,6 +32,8 @@ class URLObject():
         return self.json
     def print(self):
         print(json.dumps(self.json.json(), indent=4))
+        
+        
 secret = os.getenv("IG_ACCESS_TOKEN")
 base = "https://graph.instagram.com/v23.0/"
 igurl = URLObject(secret,base)
@@ -37,12 +43,11 @@ from minio import Minio
 
 
 min_cli = Minio("s3.vaughn.sh", os.getenv("S3_ACCESS"), os.getenv("S3_SECRET"))
-
 print(min_cli.list_buckets())
+
 from flask import Flask, request, jsonify, Response
 
 webhook = discord.SyncWebhook.from_url(os.getenv("WEBHOOK"))
-
 
 app = Flask(__name__)
 
@@ -56,7 +61,7 @@ def insta():
         if (not entry or  not entry[0]) :
             return Response(status=500, mimetype='application/json')
         mes = entry[0].get("messaging")
-        print(json.dumps(mes, indent=4))
+
         if(not mes or not mes[0]):
             return Response(status=500, mimetype='application/json')
         message = mes[0].get("message")
@@ -72,14 +77,14 @@ def insta():
         url = payload.get("url")
         title =  payload.get("title")
         stream = min_cli.list_objects("zink", "kaiba", True)
-        print(stream)
+      
         part_size = 10 * 1024 * 1024
         with urllib.request.urlopen(url) as response:
             tags = Tags(for_object=True)
             user_id = mes[0].get("sender").get("id")
             tags["User"] = user_id
             user_data = igurl.get(user_id).json()
-            print(user_data)
+           
             tags["Timestamp"] = str(mes[0].get("timestamp")) 
             # print(response.headers)
             content_type = response.headers.get_content_type().split("/")[-1]
@@ -113,16 +118,8 @@ def insta():
             
             embed = discord.Embed( description= title if title else "no title", timestamp=datetime.fromtimestamp(int(mes[0].get("timestamp"))/1000, tz=timezone("US/Eastern")))
             embed.set_author(name=user_data.get("username"),icon_url=user_data.get("profile_pic"))
-            
-            
-            print(response.status, response.url, response.headers)
-            
             webhook.send(file=f, embed=embed)
-            body = {"content":f"[content]({url})", "embeds":[embed.to_dict()]}
-            # print("E TO D", body)
-            # res = requests.post(os.getenv("WEBHOOK"), json = body)
-            # res = requests.post(os.getenv("WEBHOOK"), json = body, headers={"Content-Type":"application/json"})
-            # print(res, res.reason, res.text)
+            
         return Response(status=200, mimetype='application/json')
     if(request.method=="GET"):
         print(request.args)
@@ -132,5 +129,7 @@ def insta():
     return Response(status=400, mimetype='application/json')
         
 
+
+# for testings
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=8887)
